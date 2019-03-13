3f97b401c3b39d7cfb425077a7eb616b5d919e43
Ana Luczynski <ana.luczynski@salesforce.com>
2019-03-13 09:47:23 -0500

    add a simple form for taking notes

--- src/components/NoteTaker.jsx
+++ src/components/NoteTaker.jsx
@@ -11,16 +11,42 @@ export default class NoteTaker extends React.Component {
     super();
 
     this.state = {
-      updated: false,
-      tokenCandidate: (props.token === undefined) ? '' : props.token
+      thought: '',
+      tags: ''
     };
 
     quip.apps.updateToolbar({disabledCommandIds: []});
   }
 
+  saveThought = () =>{
+    console.log('TODO will save the thought');
+  }
+
+  updateTags = (event) => {
+    this.setState({tags: event.target.value});
+  }
+
+  updateThought = (event) => {
+    this.setState({thought: event.target.value});
+  }
+
   render() {
     return <div className={Style.noteForm}>
-      <p>Hellow werld!</p>
+      <label className={Style.stackedFormInput}>
+        <span className={Style.label}>current thought</span>
+        <textarea className={Style.thoughtInput} onInput={this.updateThought} value={this.state.thought}></textarea>
+      </label>
+
+      <div className={Style.formRow}>
+        <label className={Style.stackedFormInput}>
+          <span className={Style.label}>tags</span>
+          <input type="text" onInput={this.updateTags} value={this.state.tags} />
+        </label>
+
+        <button type="button" onClick={this.saveThought} disabled={!this.state.thought.length} className={`${Style.buttonSimple} ${Style.submitNote}`}>
+          add
+        </button>
+      </div>
     </div>;
   }
 }
