70ac79f985c6e3aa0129ad133af93646bf81ae48
Ana Luczynski <ana.luczynski@salesforce.com>
2019-03-15 11:26:37 -0500

    Create TestButtons.jsx

---
+++ src/components/TestButtons.jsx
@@ -0,0 +1,28 @@
+import QuipCaller from '../util/quip-caller.js';
+import Style from '../style/Form.less';
+
+export default class TestButtons extends React.Component {
+  static propTypes = {
+    token: React.PropTypes.string
+  };
+
+  constructor(props) {
+    super();
+
+    this.docCaller = QuipCaller(props.token, quip.apps.getThreadId());
+  }
+
+  testDocumentGetter = () => {
+    this.docCaller.getDocument().then(response => {
+      console.log(response);
+    });
+  }
+
+  render() {
+    return <p>
+      <button type="button" onClick={this.testDocumentGetter} className={Style.buttonSimple}>
+        test document getter
+      </button>
+    </p>;
+  }
+}
