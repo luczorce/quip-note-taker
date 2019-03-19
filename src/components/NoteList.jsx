98c3880537d0100adf9fa6fc847523d408850172
Ana Luczynski <ana.luczynski@salesforce.com>
2019-03-19 13:38:15 -0500

    render a list of notes that relate to certain sections

--- src/components/NoteList.jsx
+++ src/components/NoteList.jsx
@@ -1,23 +1,57 @@
-import Style from '../style/Form.less';
+import Style from '../style/Notes.less';
 import Message from '../style/Message.less';
 
-export default class NoteTaker extends React.Component {
+export default class NoteList extends React.Component {
   static propTypes = {
     notes: React.PropTypes.array,
-    currentSection= React.PropTypes.string
+    currentSections: React.PropTypes.array
   };
 
   // constructor(props) {
   //   super();
   // }
 
-  render() {
-    const currentNotes = this.state.notes.filter(n => n.topics.includes(this.state.currentSection));
+  getCurrentNotes = () => {
+    return this.props.notes.filter(n => {
+      let isPresent = false;
+      this.props.currentSections.forEach(s => {
+        if (n.topics.includes(s)) {
+          isPresent = true;
+        }
+      });
+
+      return isPresent;
+    });
+  }
+
+  makeEachNote = (note) => {
+    let topics = note.topics.filter(t => t !== this.props.currentSection);
     
-    const notes = currentNotes.reverse.forEach((n, index) => {
-      return <div key={index}
+    topics = topics.map(t => {
+      if (t[0] === '#') {
+        return <span className={Style.noteTopic}>{t}</span>;
+      } else {
+        return <span className={Style.noteSection}>{t}</span>;
+      }
     });
 
-    return <div>{notes}</div>;
+    return <div key={note.guid} className={Style.note}>
+      <textarea value={note.content} disabled="true"></textarea>
+      <div className={Style.topicList}>{topics}</div>
+      <div className={Style.owner}>owner: {note.owner}</div>
+    </div>;
+  }
+
+  render() {
+    const currentNotes = this.getCurrentNotes();
+    let notes;
+
+    if (currentNotes.length) {
+      notes = currentNotes.reverse().map(this.makeEachNote);
+    } else {
+      notes = <p>add notes to get started</p>;
+    }
+
+    return <div className={Style.noteList}>{notes}</div>;
   }
 }
